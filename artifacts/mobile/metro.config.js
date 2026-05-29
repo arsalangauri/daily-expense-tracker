0b9e7105e77582449dd7d6d5b03e4e97ff15d5a9
arsalangauri <arsalangaurione@gmail.com>
2026-05-29 10:24:10 +0500

    Fix EAS Build: artifacts/mobile/metro.config.js

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,3 +1,23 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-module.exports = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver = {
+  ...config.resolver,
+  blockList: [
+    // Ignore tmp directories created by postinstall scripts (esbuild-plugin-pino, etc.)
+    /node_modules\/.*_tmp_\d+/,
+    /node_modules\/[^/]+_tmp_[^/]+/,
+    // Ignore other artifacts' node_modules to avoid conflicts
+    /artifacts\/api-server\/node_modules/,
+    /artifacts\/mockup-sandbox\/node_modules/,
+  ],
+};
+
+module.exports = config;
